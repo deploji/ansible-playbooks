bc7db9f67c28a97dc64ba402d4002f7c64755e5f
Robert Sotomski <sotomski@gmail.com>
2019-10-09 23:04:47 +0200

    add deploy.sh

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Deploji ansible-playbooks
+
+## Deploy Deploji on Ubuntu server
+1. Edit `inventories/test.ini` file. Change `ansible_host` variable in to your server IP
+2. Execute `./deploy.sh`
---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker-compose run ansible-playbook -i inventories/test.ini -e ansible_user=${USER} playbooks/docker_ubuntu.yml
+docker-compose run ansible-playbook -i inventories/test.ini -e ansible_user=${USER} -e app=traefik -e version=1.7 playbooks/deploy.yml
+docker-compose run ansible-playbook -i inventories/test.ini -e ansible_user=${USER} -e app=deploji-server -e version=latest playbooks/deploy.yml
+docker-compose run ansible-playbook -i inventories/test.ini -e ansible_user=${USER} -e app=deploji-worker -e version=latest playbooks/deploy.yml
+docker-compose run ansible-playbook -i inventories/test.ini -e ansible_user=${USER} -e app=deploji -e version=latest playbooks/deploy.yml
